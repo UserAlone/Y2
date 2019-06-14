21b4c8aafc4588cd705023f60ce144cd8587ed0b
周波 <@LAPTOP-6L62MNBB>
2019-06-14 17:03:38 +0800

    db

---
+++ Y2/BAL/SqlHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BAL
+{
+    public class SqlHelper
+    {
+        //获取连接对象
+        public static SqlConnection GetSqlconnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["HR_DB "].ConnectionString);
+        }
+
+
+        //增删改
+        public static int MyExecuteNonQuery(string sql, SqlParameter[] sps)
+        {
+            using (SqlConnection sqlconn = GetSqlconnection())
+            {
+                SqlCommand sqlcmd = new SqlCommand(sql, sqlconn);
+                sqlcmd.CommandType = CommandType.Text;
+                if (sps != null)
+                {
+                    sqlcmd.Parameters.AddRange(sps);
+                }
+                sqlconn.Open();
+                return sqlcmd.ExecuteNonQuery();
+            }
+        }
+
+        //读取器
+        public static SqlDataReader MyReader(string sql, SqlParameter[] sps)
+        {
+            SqlConnection sqlconn = GetSqlconnection();
+
+            SqlCommand sqlcmd = new SqlCommand(sql, sqlconn);
+            sqlcmd.CommandType = CommandType.Text;
+            if (sps != null)
+            {
+                sqlcmd.Parameters.AddRange(sps);
+            }
+            sqlconn.Open();
+            return sqlcmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+        }
+
+        //适配器
+        public static DataTable MyAdapter(string sql, SqlParameter[] sps)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection sqlconn = GetSqlconnection())
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                if (sps != null)
+                {
+                    adapter.Fill(dt);
+                }
+                return dt;
+            }
+        }
+
+        //返回单个值
+        public static object MyScalar(string sql, SqlParameter[] sps)
+        {
+            using (SqlConnection sqlconn = GetSqlconnection())
+            {
+                SqlCommand sqlcmd = new SqlCommand(sql, sqlconn);
+                sqlcmd.CommandType = CommandType.Text;
+                if (sps != null)
+                {
+                    sqlcmd.Parameters.AddRange(sps);
+                }
+                sqlconn.Open();
+                return sqlcmd.ExecuteScalar();
+            }
+        }
+    }
+}
