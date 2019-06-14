2fbd6e0f447644de9dc3cbce0124a54d69d286c0
UserAlone <1270267713@qq.com>
2019-06-14 15:50:57 +0800

    实体类

---
+++ Y2/UI/Controllers/clientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class clientController : Controller
+    {
+        // GET: client
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
